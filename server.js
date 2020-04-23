36babef68eb837bd18cff935646e034673317c88
matibejeremy <matibejeremy@gmail.com>
2020-04-23 17:58:13 +0300

    Trying to fix heroku error

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/e-giftvocha'));
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+
-        '/dist/e-giftvocha/index.html'));});
+app.use(express.static(__dirname + '/dist/e-giftvocha/'));
+app.get('*', (req, res) => {
+    res.sendFile(`./dist/e-giftvocha/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+});
 app.listen(process.env.PORT || 8080);
+
