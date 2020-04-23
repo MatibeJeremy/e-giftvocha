697a60cb6d4f96c9d12c344e5e952207c2939d93
matibejeremy <matibejeremy@gmail.com>
2020-04-23 17:14:52 +0300

    Configs

---
+++ src/app/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    name: 'Tuskeys Shopping Vouchers',
+    price: 799,
+    description: 'The price indicated is the value in vouchers you can buy according to your float balance. Click to explore'
+  },
+  {
+    name: 'Mama Mboga Groceries',
+    price: 699,
+    description: 'Please note that these vouchers can only be redeemed at the designated places. With reports available to you at your request'
+  },
+  {
+    name: 'Niavas Supermarket',
+    price: 299,
+    description: 'Stocks are running low. Buy now!'
+  },
+  {
+    name: 'Tidys Restaurant',
+    price: 5454,
+    description: 'Your beneficiaries can enjoy any meal from this restaurant.'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
